e00cf3fd8ff600ee9216ae389c7a01c8429a57c9
Keith Worsham <worsham.keith@gmail.com>
2026-07-04 16:01:53 -0400

    Update layout and home page structure with Header and Footer components, and revise introductory text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
+
+
+
+
+
+
+
+
+
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -14,10 +24,9 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
+            My Portfolio</h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            I am a full-stack developer learning Next.js and React. Here are some of my recent projects: {" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
