c0ec9e279d0f0eb1af74feb00e3a8ac777fefb10
Keith Worsham <worsham.keith@gmail.com>
2026-07-17 21:20:21 -0400

    Step 7 School Page update

--- app/projects/school/page.tsx
+++ app/projects/school/page.tsx
@@ -1,40 +1,67 @@
-interface Project {
-  id: number;
-  title: string;
-  description: string;
-  type: 'opensource' | 'school';
-  technologies: string[];
-  link?: string;
-}
+import ProjectList from '../../../components/projectList';
 
-async function getProjects(): Promise<Project[]> {
-  const res = await fetch(
-    'http://localhost:3000/api/projects?type=school',
-    {
-      cache: 'no-store',
-    }
-  );
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch projects');
-  }
-
-  return res.json();
-}
+import { fetchProjects } from '../lib/fetch-projects';
 
 export default async function SchoolProjectsPage() {
-  const projects = await getProjects();
+    const projects = await fetchProjects('school');
 
-  return (
-    <main>
-      <h1>School Projects</h1>
-
-      {projects.map((project) => (
-        <article key={project.id}>
-          <h2>{project.title}</h2>
-          <p>{project.description}</p>
-        </article>
-      ))}
-    </main>
-  );
+    return <ProjectList projects={projects} />;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// interface Project {
+//   id: number;
+//   title: string;
+//   description: string;
+//   type: 'opensource' | 'school';
+//   technologies: string[];
+//   link?: string;
+// }
+
+// async function getProjects(): Promise<Project[]> {
+//   const res = await fetch(
+//     'http://localhost:3000/api/projects?type=school',
+//     {
+//       cache: 'no-store',
+//     }
+//   );
+
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch projects');
+//   }
+
+//   return res.json();
+// }
+
+// export default async function SchoolProjectsPage() {
+//   const projects = await getProjects();
+
+//   return (
+//     <main>
+//       <h1>School Projects</h1>
+
+//       {projects.map((project) => (
+//         <article key={project.id}>
+//           <h2>{project.title}</h2>
+//           <p>{project.description}</p>
+//         </article>
+//       ))}
+//     </main>
+//   );
+// }
